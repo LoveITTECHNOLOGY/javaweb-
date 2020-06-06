b25e5a0a0ad217d4f200fda54f456dafa6788c4e
jiafu wei <62496045+weijiafu19981002@users.noreply.github.com>
2020-06-06 11:48:49 +0800

    Add files via upload

---
+++ 第四节简化前端异步处理和连接数据库操作/在oracle操作所用的数据库文件/first.sql
@@ -0,0 +1,10 @@
+create table student(
+       id number(10),
+       age number(10),
+       name varchar2(20),
+       sex varchar2(20),
+       constraint student_id primary key(id)
+
+)
+select * from student;
+insert into student values(2,18,'Ğ¡¸Õ','ÄĞ');
